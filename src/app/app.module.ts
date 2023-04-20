8d2f352d7b4ebd3b3c63c69b9fcc7c1635ce23d7
Alexis Gonzalez <1991.alexis@gmail.com>
2023-04-20 17:29:30 -0400

    Added styles to container component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { KillerCompComponent } from './components/killer-comp/killer-comp.component';
 import { KillerCotainerComponent } from './components/killer-cotainer/killer-cotainer.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    KillerCompComponent,
-    KillerCotainerComponent
-  ],
+  declarations: [AppComponent, KillerCompComponent, KillerCotainerComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
