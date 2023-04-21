2e2b3a2cc1ddba4a2ba8abc38cae04ea64ec3f7b
Alexis Gonzalez <1991.alexis@gmail.com>
2023-04-21 10:10:13 -0400

    Can take snapshots =)

--- src/app/components/killer-cotainer/killer-cotainer.component.ts
+++ src/app/components/killer-cotainer/killer-cotainer.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import html2canvas from 'html2canvas';
+import { jsPDF } from 'jspdf';
 
 @Component({
   selector: 'app-killer-cotainer',
   templateUrl: './killer-cotainer.component.html',
-  styleUrls: ['./killer-cotainer.component.scss']
+  styleUrls: ['./killer-cotainer.component.scss'],
 })
 export class KillerCotainerComponent implements OnInit {
+  image: any;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  async takeSnapShot() {
+    // @ts-ignore
+    const aux = await html2canvas(document.querySelector('#capture'));
+    this.image = aux.toDataURL();
   }
 
+  eraseSnapShot() {
+    this.image = null;
+  }
 }
